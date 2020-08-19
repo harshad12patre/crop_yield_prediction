bf54f1d519555b42d7fc32ebd958b391ea07b59d
Harshad Barapatre <harshadbarapatre@hotmail.com>
2020-08-20 01:04:05 +0530

    Updates

--- readdata.R
+++ readdata.R
@@ -1,9 +1,25 @@
+library(ggplot2)
 library(dplyr)
 library(tidyverse)
 
-data <- read.csv("./ds/BigDatafin.csv")
+data <- read.csv("./ds/rainfall_monthwise(2010-2016).csv")
 
-datnew <- data %>%
+data <- data %>%
+  mutate(rain = as.integer(rain)) 
+
+sum(is.na(data$rain))
+
+data %>%
+  group_by(month) %>%
+  summarize(meanm = mean(rain))
+
+nskdat <- data %>% 
   filter(district == "Nasik") %>%
-  select(district, month, year, rain) %>%
-  group_by(year, month)
+  group_by(month) %>%
+  summarize(monthm = mean(rain))
+
+nskdat <- nskdat %>%
+  mutate(month)
+
+nskdat %>%
+  ggplot(aes(monthm, month), color = Blue) + geom_line()
